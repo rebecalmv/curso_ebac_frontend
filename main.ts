675467fd5b6b6ed62e9d9fcb50b92bc0cb61b6df
Rebeca Lima <124937335+rebecalmv@users.noreply.github.com>
2023-07-16 12:27:45 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+//funcao de multiplicacao
+
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+console.log(multiplicacao(2, 2));
+
+
+
+//funcao de saudacao
+
+function saudacao(nome: string): string {
+    return `Olá, ${nome}!`;
+}
+
+const nomeSaudacao = saudacao("Rebeca");
+console.log(nomeSaudacao);
